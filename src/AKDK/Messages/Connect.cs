fdcb6202524b096769fef94116ee930c13d052c0
Adam Friedman <tintoy@tintoy.io>
2017-05-08 13:42:39 +1000

    Sketch out TCP connection API.

--- src/AKDK/Messages/Connect.cs
+++ src/AKDK/Messages/Connect.cs
@@ -1,15 +1,33 @@
-﻿using AKDK.Utilities;
-using Docker.DotNet;
+﻿using Docker.DotNet;
 using System;
 
 namespace AKDK.Messages
 {
+    using Utilities;
+
     /// <summary>
     ///     Request to create a connection to a Docker API.
     /// </summary>
     public class Connect
         : Request
     {
+        /// <summary>
+        ///     Create a new <see cref="Connect"/> message.
+        /// </summary>
+        /// <param name="endpointUri">
+        ///     The end-point URI for the Docker API.
+        /// </param>
+        /// <param name="credentials">
+        ///     Optional credentials for authenticating to the Docker API.
+        /// </param>
+        /// <param name="correlationId">
+        ///     An optional message correlation Id.
+        /// </param>
+        public Connect(string endpointUri, Credentials credentials = null, string correlationId = null)
+            : this(new Uri(endpointUri), credentials, correlationId)
+        {
+        }
+
         /// <summary>
         ///     Create a new <see cref="Connect"/> message.
         /// </summary>
@@ -32,12 +50,12 @@ namespace AKDK.Messages
         /// <summary>
         ///     The end-point URI for the Docker API.
         /// </summary>
-        public Uri EndpointUri;
+        public Uri EndpointUri { get; }
 
         /// <summary>
         ///     Optional credentials for authenticating to the Docker API.
         /// </summary>
-        public Credentials Credentials;
+        public Credentials Credentials { get; }
 
         /// <summary>
         ///     Create a request to connect to the local Docker API.
@@ -48,6 +66,23 @@ namespace AKDK.Messages
         /// <returns>
         ///     The <see cref="Connect"/> request.
         /// </returns>
-        public static Connect Local(string correlationId) => new Connect(LocalDocker.EndPointUri);
+        public static Connect Local(string correlationId = null) => new Connect(LocalDocker.EndPointUri);
+
+        /// <summary>
+        ///     Create a request to connect to a Docker API over TCP.
+        /// </summary>
+        /// <param name="hostName">
+        ///     The target docker host name.
+        /// </param>
+        /// <param name="port">
+        ///     The target TCP port.
+        /// </param>
+        /// <param name="correlationId">
+        ///     An optional message correlation Id.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="Connect"/> request.
+        /// </returns>
+        public static Connect Tcp(string hostName, int port = 2375, string correlationId = null) => new Connect(new Uri($"tcp://{hostName}:{port}"));
     }
 }
---
+++ src/AKDK/Messages/Connected.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using System;
+
+namespace AKDK.Messages
+{
+    /// <summary>
+    ///     Response indicating a successful connection to a Docker API.
+    /// </summary>
+    public class Connected
+        : Response
+    {
+        /// <summary>
+        ///     Create a new <see cref="Connected"/> message.
+        /// </summary>
+        /// <param name="client">
+        ///     The client actor for the connection.
+        /// </param>
+        /// <param name="endpointUri">
+        ///     The end-point URI for the Docker API.
+        /// </param>
+        /// <param name="correlationId">
+        ///     The message correlation Id.
+        /// </param>
+        public Connected(IActorRef client, Uri endpointUri, string correlationId)
+            : base(correlationId)
+        {
+            Client = client;
+            EndpointUri = endpointUri;
+        }
+
+        /// <summary>
+        ///     The client actor for the connection.
+        /// </summary>
+        public IActorRef Client { get; }
+
+        /// <summary>
+        ///     The end-point URI for the Docker API.
+        /// </summary>
+        public Uri EndpointUri { get; }
+    }
+}
