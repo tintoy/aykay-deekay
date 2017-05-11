e78a1c91a032e06fb140b707f6c68e7952d762bc
Adam Friedman <tintoy@tintoy.io>
2017-05-11 16:52:10 +1000

    Add extension methods to determine whether an actor is an ancestor or descendant of another actor.

---
+++ src/AKDK/Utilities/AkkaExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Akka.Actor;
+using System;
+
+namespace AKDK.Utilities
+{
+    /// <summary>
+    ///     Extension methods for Akka types.
+    /// </summary>
+    public static class AkkaExtensions
+    {
+        /// <summary>
+        ///     Determine whether another actor is a descendent of the actor.
+        /// </summary>
+        /// <param name="actor">
+        ///     The actor.
+        /// </param>
+        /// <param name="otherActor">
+        ///     The other actor.
+        /// </param>
+        /// <returns>
+        ///     <c>true</c>, if <paramref name="otherActor"/> is a descendant of <paramref name="actor"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsDescendantOf(this IActorRef actor, IActorRef otherActor)
+        {
+            if (actor == null)
+                throw new ArgumentNullException(nameof(actor));
+
+            if (otherActor == null)
+                throw new ArgumentNullException(nameof(otherActor));
+
+            ActorPath parentPath = actor.Path;
+            ActorPath otherParentPath = otherActor.Path.Parent;
+            ActorPath rootPath = otherActor.Path.Root;
+            while (otherParentPath != rootPath)
+            {
+                if (otherParentPath == parentPath)
+                    return true;
+
+                otherParentPath = otherParentPath.Parent;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///     Determine whether another actor is an ancestor of the actor.
+        /// </summary>
+        /// <param name="actor">
+        ///     The actor.
+        /// </param>
+        /// <param name="otherActor">
+        ///     The other actor.
+        /// </param>
+        /// <returns>
+        ///     <c>true</c>, if <paramref name="otherActor"/> is an ancestor of <paramref name="actor"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsAncestorOf(this IActorRef actor, IActorRef otherActor)
+        {
+            if (actor == null)
+                throw new ArgumentNullException(nameof(actor));
+
+            if (otherActor == null)
+                throw new ArgumentNullException(nameof(otherActor));
+
+            return otherActor.IsDescendantOf(actor);
+        }
+    }
+}
