6001fda86bbf5a926eb615e4d9a25d920f7f57bf
Adam Friedman <tintoy@tintoy.io>
2017-05-12 15:44:53 +1000

    Create ConnectionTestKit and add a couple of initial tests for the Client actor.

---
+++ test/AKDK.Tests/ClientTests.cs
@@ -0,0 +1,65 @@
+﻿using Akka.Actor;
+using Docker.DotNet.Models;
+using System.Linq;
+using Xunit;
+
+namespace AKDK.Tests
+{
+    using Actors;
+    using Messages;
+
+    /// <summary>
+    ///     Tests for the <see cref="Client"/> actor.
+    /// </summary>
+    public class ClientTests
+        : ConnectionTestKit
+    {
+        public ClientTests()
+            : base(actorSystemName: "client-tests")
+        {
+        }
+
+        [Fact]
+        public void Client_Create_SendsNoMessageToConnection()
+        {
+            IActorRef client = CreateClient();
+            ConnectionTestProbe.ExpectNoMsg();
+        }
+
+        [Fact]
+        public void Client_ListImages_Success()
+        {
+            IActorRef client = CreateClient();
+            client.Tell(
+                new ListImages(new ImagesListParameters(),
+                    correlationId: nameof(Client_ListImages_Success)
+                )
+            );
+            ConnectionTestProbe.ExpectMsg<Connection.ExecuteCommand>(executeCommand =>
+            {
+                Assert.IsType<ListImages>(executeCommand.RequestMessage);
+                Assert.Equal(
+                    expected: nameof(Client_ListImages_Success),
+                    actual: executeCommand.CorrelationId
+                );
+                Assert.Equal(
+                    expected: nameof(Client_ListImages_Success),
+                    actual: executeCommand.RequestMessage.CorrelationId
+                );
+
+                ConnectionTestProbe.Reply(new ImageList(executeCommand.CorrelationId,
+                    images: Enumerable.Empty<ImagesListResponse>()
+                ));
+            });
+
+            ExpectMsg<ImageList>(imageList =>
+            {
+                Assert.Equal(
+                    expected: nameof(Client_ListImages_Success),
+                    actual: imageList.CorrelationId
+                );
+                Assert.Equal(0, imageList.Images.Count);
+            });
+        }
+    }
+}
---
+++ test/AKDK.Tests/ConnectionTestKit.cs
@@ -0,0 +1,62 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using Akka.TestKit;
+using Akka.TestKit.Xunit2;
+
+namespace AKDK.Tests
+{
+    using Actors;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    ///     The base class for actor tests that require a simulated <see cref="Connection"/> actor.
+    /// </summary>
+    public abstract class ConnectionTestKit
+        : TestKit
+    {
+        /// <summary>
+        ///     The Akka configuration used by the tests.
+        /// </summary>
+        protected static readonly Config TestConfig =
+            ConfigurationFactory.ParseString(@"
+                akka.suppress-json-serializer-warning = on
+            ")
+            .WithFallback(DefaultConfig);
+
+        /// <summary>
+        ///     Create a new <see cref="ConnectionTestKit"/>.
+        /// </summary>
+        /// <param name="actorSystemName">
+        ///     The name of the actor system used to host the tests.
+        /// </param>
+        protected ConnectionTestKit(string actorSystemName)
+            : base(TestConfig, actorSystemName)
+        {
+            ConnectionTestProbe = CreateTestProbe(name: "connection");
+        }
+
+        /// <summary>
+        ///     The <see cref="TestProbe"/> representing the <see cref="Connection"/> actor.
+        /// </summary>
+        protected TestProbe ConnectionTestProbe { get; }
+
+        /// <summary>
+        ///     Create a new <see cref="Client"/> actor that uses <see cref="ConnectionTestProbe"/>.
+        /// </summary>
+        /// <param name="name">
+        ///     An optional name for the <see cref="Client"/> actor (defaults to "client" if not specified).
+        /// </param>
+        /// <returns>
+        ///     A reference to the <see cref="Client"/> actor.
+        /// </returns>
+        protected virtual IActorRef CreateClient(string name = null)
+        {
+            return Sys.ActorOf(
+                Props.Create(
+                    () => new Client(ConnectionTestProbe)
+                ),
+                name: name ?? "client"
+            );
+        }
+    }
+}
